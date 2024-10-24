4a7dcae5131cc629dd1cb6f9d90b43fbfb9b187d
inHansZone <han.lehoangngoc@hcmut.edu.vn>
2024-10-24 16:25:11 +0700

    Delete src directory

--- src/controllers/admin/admin_controller.go
+++
@@ -1,5 +0,0 @@
-package controllers_admin
-
-import "github.com/gin-gonic/gin"
-
-func CreateAdmin(c *gin.Context) {}
--- src/routes/admin/class_route.go
+++
@@ -1,9 +0,0 @@
-package routes_admin
-
-import(
-	controllers_admin "be-go-2/controllers/admin"
-	"github.com/gin-gonic/gin"
-)
-func ClassRoutes(r *gin.RouterGroup){
-	r.POST("/class/create",controllers_admin.CreateClass)
-}
