ef276c07a0945ef950ebe23280fe8ce258452c4f
nguyentienphat2904 <phat.nguyen221252729@hcmut.edu.vn>
2024-12-01 17:25:47 +0700

    feature/createAdmin: basic add new accounts

--- src/controllers/admin/AccountController.go
+++ src/controllers/admin/AccountController.go
@@ -1,12 +1,91 @@
 package controller_admin
 
 import (
+	"Go2/models"
+	"context"
+
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/v2/bson"
 )
 
 // HandleCreateAccount xử lý việc tạo tài khoản mới.
 func HandleCreateAccount(c *gin.Context) {
+	createdBy, _ := c.Get("ID") // Get creator's ID
 
+	var newAccounts []InterfaceAccount
+
+	// Check if the body of the request is valid
+	if err := c.ShouldBindJSON(&newAccounts); err != nil {
+		c.JSON(400, gin.H{
+			"status":  "Fail",
+			"message": "Invalid data",
+		})
+		return
+	}
+
+	// Check dupplicate
+	accountCol := models.AccountModel()
+	var existedAccounts []models.InterfaceAccount
+
+	cusor, err := accountCol.Find(context.TODO(), bson.M{})
+
+	if err != nil { // Error when retrieve database
+		c.JSON(500, gin.H{
+			"status":  "Fail",
+			"message": "Error when retrieve on database",
+		})
+		return
+	}
+
+	if err := cusor.All(context.TODO(), &existedAccounts); err != nil {
+		c.JSON(500, gin.H{
+			"status":  "Fail",
+			"message": "Error decoding account",
+		})
+		return
+	}
+
+	// Arrays to store dupplicating email and id
+	var emailSet, idSet []string
+	for _, account := range existedAccounts {
+		emailSet = append(emailSet, account.Email)
+		idSet = append(idSet, account.Ms)
+	}
+	// Classify valid and invalid accounts
+	var validAccounts, invalidAccounts []InterfaceAccount
+	for _, account := range newAccounts {
+		if contains(emailSet, account.Email) || contains(idSet, account.Ms) {
+			invalidAccounts = append(invalidAccounts, account)
+		} else {
+			validAccounts = append(validAccounts, account)
+		}
+	}
+
+	// Add valid accounts to database
+	if len(validAccounts) > 0 {
+		_, err := accountCol.InsertMany(context.TODO(), validAccounts)
+		if err != nil {
+			c.JSON(500, gin.H{
+				"status":  "Fail",
+				"message": "Error when creating account",
+			})
+			return
+		}
+	}
+	c.JSON(200, gin.H{
+		"status":          "success",
+		"invalidAccounts": invalidAccounts,
+		"validAccount":    validAccounts,
+	})
+}
+
+func contains(slice []string, value string) bool {
+	for _, v := range slice {
+		if v == value {
+			return true
+		}
+	}
+	return false
 }
 
 // CheckEmailAndRole kiểm tra đuôi email và role
--- src/routes/admin/AccountRoute.go
+++ src/routes/admin/AccountRoute.go
@@ -1,10 +1,28 @@
 package routes_admin
 
 import (
+	controller_admin "Go2/controllers/admin"
+
 	"github.com/gin-gonic/gin"
 )
 
 // AccountRoute thiết lập các route cho tài khoản
 func AccountRoute(r *gin.RouterGroup) {
+	// Create new admin account
+	r.POST("/create", controller_admin.HandleCreateAccount)
 
+	// Find admin account by ID
+	r.GET("/:id", controller_admin.HandleGetAccountByID)
+
+	// Get all teacher accounts
+	r.GET("/teacher", controller_admin.HandleGetTeacherAccounts)
+
+	// Get all student accounts
+	r.GET("/student", controller_admin.HandleGetStudentAccounts)
+
+	// Delete account by ID
+	r.DELETE("/delete/:id", controller_admin.HandleDeleteAccount)
+
+	// Update account by ID
+	r.PATCH("/change/:id", controller_admin.HandleUpdateAccount)
 }
